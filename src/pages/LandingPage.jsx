7f70c0c1cb1cb86218d80465ec88d331500e3fbc
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-20 15:25:03 +0100

    latest push

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,7 @@
 /*
  * ============================================================
  *  LandingPage.jsx
- *  Self-contained — imports only global.css (via maisn.jsx)
+ *  Self-contained — imports only global.css (via main.jsx)
  *  and its own LandingPage.css
  * ============================================================
  */
