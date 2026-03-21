99ed99539b5bac6eceec233b74d75cb393b3d1aa
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-21 15:08:25 +0100

    latest push

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -25,7 +25,7 @@ import { calcPrice, fmtNaira, getBreakdown, COMPANIES } from '../utils/pricing'
 
 const MAX_COMPANIES = 1223
 
-/* heeelper*/
+/* helper*/
 function scrollTo(id) {
   document.getElementById(id)?.scrollIntoView({ behavior: 'smooth', block: 'start' })
 }
