46601106d45a9c0932ed66cee3b5ecc2a5931c45
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-30 16:24:33 +0100

    latest update

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -16,16 +16,16 @@ const BACKEND_URL = import.meta.env.VITE_API_URL
   : 'http://localhost:5001'
 
 export default function Auth({ defaultTab = 'login', onLogin, onBack }) {
-  const [tab, setTab] = useState(defaultTab)
-  const [googleLoading, setGoogleLoading] = useState(false)
-  const { showToast } = useToast()
+  // const [tab, setTab] = useState(defaultTab)
+  // const [googleLoading, setGoogleLoading] = useState(false)
+  // const { showToast } = useToast()
 
-  const [loginEmail, setLoginEmail] = useState('')
-  const [loginPass, setLoginPass] = useState('')
+  // const [loginEmail, setLoginEmail] = useState('')
+  // const [loginPass, setLoginPass] = useState('')
 
-  const [signupName, setSignupName] = useState('')
-  const [signupEmail, setSignupEmail] = useState('')
-  const [signupPass, setSignupPass] = useState('')
+  // const [signupName, setSignupName] = useState('')
+  // const [signupEmail, setSignupEmail] = useState('')
+  // const [signupPass, setSignupPass] = useState('')
 
 
   function handleSignup() {
